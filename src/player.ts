7533963cb9664ca8c41cc1a177009c59ace70271
ckieverentineas <ckieverentineas@yandex.ru>
2023-07-24 23:49:58 +1000

    add command statisctis

--- src/player.ts
+++ src/player.ts
@@ -1,6 +1,7 @@
 import { HearManager } from "@vk-io/hear";
 import { root, vk } from "./index";
 import { IQuestionMessageContext } from "vk-io-question";
+import prisma from "./module/prisma";
 
 
 export function registerUserRoutes(hearManager: HearManager<IQuestionMessageContext>): void {
@@ -14,7 +15,40 @@ export function registerUserRoutes(hearManager: HearManager<IQuestionMessageCont
             })
         }
     })
-    /*hearManager.hear(/стата/, async (context: any) => {
-        await context.send()
-    })*/
+    hearManager.hear(/стата/, async (context: any) => {
+        let users = 'Рейтинг по добытой энергии\n\n'
+        let counter = 1
+        let user_me = null
+        for (const user of await prisma.analyzer.findMany({ orderBy: { energy: 'desc' }, include: { user: true } })) {
+            if (counter <= 10) {
+                users += `${counter} - @id${user.user.idvk}(${user.user.name.slice(0, 20)}) --> ${user.energy.toFixed(2)}⚡\n`
+            }
+            if (user.user.idvk == context.senderId) { user_me = `\n${counter} - @id${user.user.idvk}(${user.user.name.slice(0, 20)}) --> ${user.energy.toFixed(2)}⚡`}
+            counter++
+        }
+        /*const text = [
+            { idvk: 1, id: 1, text: "Г", white: " " },
+            { idvk: 12, id: 1, text: "ГИ", white: " " },
+            { idvk: 123, id: 1, text: "ГИЛ", white: " " },
+            { idvk: 1234, id: 1, text: "ГИЛЬ", white: " " },
+            { idvk: 12345, id: 1, text: "ГИЛЬД", white: " " },
+            { idvk: 123456, id: 1, text: "ГИЛЬДИ", white: " " },
+            { idvk: 1234567, id: 1, text: "ГИЛЬДИЯ", white: " " }
+        ]
+        let res = []
+        //const speca = ["ᅠ", " ", " ", "　", " ", " ", "", " "]
+        const speca = ["ㅤ"]
+        for (const j in speca) {
+            for (const i in text) {
+                const calc = text[i].text.length < 8 ? 8-text[i].text.length : 0
+                const data = `${i}.${(text[i].text).slice(-8)}${text[i].white.repeat(calc)+text[i].white.repeat(3)}${String(text[i].idvk)}🎖`
+                res.push({ test: data.replace(/ /g, `${speca[j]}`)})
+            }
+            res.push({ test: speca[j] })
+        }
+        await context.send(res.map((item: { test: any; }) => {return item.test;}).join("\r\n"))
+        //console.log(res.map((item: { test: any; }) => {return item.test;}).join("\r\n"))*/
+        users += user_me
+        await context.send(`${users}.`)
+    })
 }
