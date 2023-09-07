2591737b8c0a486adce614726140d6c169ccc33b
ckieverentineas <ckieverentineas@yandex.ru>
2023-09-08 00:06:23 +1000

    starting work for powerstations

--- src/module/game/player/service.ts
+++ src/module/game/player/service.ts
@@ -8,8 +8,8 @@ export async function Time_Controller(context: Context, user: User, id_planet: n
     for (const builder of await prisma.builder.findMany({ where: { id_user: user.id, id_planet: id_planet } })) {
         const config: Builder_Selector = {
             'Шахты': Mine_Controller,
-            /*'Электростанция': Powerstation_Controller,
-            'Солнечная электростанция': Powerstation_Solar_Controller,
+            'Электростанция': Powerstation_Controller,
+            /*'Солнечная электростанция': Powerstation_Solar_Controller,
             'Центробанк': Central_Bank_Controller,
             'Археологический центр': Archaeological_Center_Controller,
             'Лаборатория': Laboratory_Controller,
@@ -27,6 +27,20 @@ export async function Time_Controller(context: Context, user: User, id_planet: n
 type Builder_Selector = {
     [key: string]: (context: Context, user: User, builder: Builder, id_planet: number) => Promise<void>;
 }
+
+async function Resource_Finder_Nafig(input_storage: Input[], input_mine: Input, target: string, datenow: Date, dateold: Date, global_koef: number) {
+    const data = { income: 0, counter: 0 }
+    for (let i=0; i < input_storage.length; i++) {
+        const store = input_storage[i]
+        if (store.name == target) {
+            console.log(`${input_mine.name}: ${input_mine.income} * (${Number(datenow)} - ${Number(dateold)})/${input_mine.time}*${global_koef}=${input_mine.income * (Number(datenow)-Number(dateold))/input_mine.time*global_koef}`)
+            data.income = input_mine.income * (Number(datenow)-Number(dateold))/input_mine.time*global_koef
+            data.counter = i
+        }
+    }
+    return data
+}
+
 async function Mine_Controller(context: Context, user: User, builder: Builder, id_planet: number) {
     const storage: Builder | null = await prisma.builder.findFirst({ where: { id_user: user.id, id_planet: id_planet, name: 'Склад' } })
     if (!storage) { await Send_Message(context.peerId, 'Але, у вас склада нет на базе, вы дома?'); return }
@@ -46,50 +60,38 @@ async function Mine_Controller(context: Context, user: User, builder: Builder, i
         }
     }
     //let event_logger = ``
-    async function Resource_Finder_Nafig(input_storage: Input[], input_mine: Input, target: string) {
-        const data = { income: 0, counter: 0 }
-        for (let i=0; i < input_storage.length; i++) {
-            const store = input_storage[i]
-            if (store.name == target) {
-                console.log(`${input_mine.name}: ${input_mine.income} * (${Number(datenow)} - ${Number(dateold)})/${input_mine.time}*${global_koef}=${input_mine.income * (Number(datenow)-Number(dateold))/input_mine.time*global_koef}`)
-                data.income = input_mine.income * (Number(datenow)-Number(dateold))/input_mine.time*global_koef
-                data.counter = i
-            }
-        }
-        return data
-    }
     const income_wil = { coal: 0, gas: 0, oil: 0, slate: 0, turf: 0, uranium: 0, iron: 0, golden: 0, artefact: 0, crystal: 0 }
     for (const input of inputs_mine) {
         if (input.name == 'golden') {
-            const data = await Resource_Finder_Nafig(inputs_storage, input, 'golden')
+            const data = await Resource_Finder_Nafig(inputs_storage, input, 'golden', datenow, dateold, global_koef)
             if ( data.income < planet.golden ) {
                 inputs_storage[data.counter].income += data.income
                 income_wil.golden += data.income
             }
         }
         if (input.name == 'iron') {
-            const data = await Resource_Finder_Nafig(inputs_storage, input, 'iron')
+            const data = await Resource_Finder_Nafig(inputs_storage, input, 'iron', datenow, dateold, global_koef)
             if ( data.income < planet.iron ) {
                 inputs_storage[data.counter].income += data.income
                 income_wil.iron += data.income
             }
         }
         if (input.name == 'coal') {
-            const data = await Resource_Finder_Nafig(inputs_storage, input, 'coal')
+            const data = await Resource_Finder_Nafig(inputs_storage, input, 'coal', datenow, dateold, global_koef)
             if ( data.income < planet.coal ) {
                 inputs_storage[data.counter].income += data.income
                 income_wil.coal += data.income
             }
         }
         if (input.name == 'crystal') {
-            const data = await Resource_Finder_Nafig(inputs_storage, input, 'crystal')
+            const data = await Resource_Finder_Nafig(inputs_storage, input, 'crystal', datenow, dateold, global_koef)
             if ( data.income < planet.crystal ) {
                 inputs_storage[data.counter].income += data.income
                 income_wil.crystal += data.income
             }
         }
         if (input.name == 'artefact') {
-            const data = await Resource_Finder_Nafig(inputs_storage, input, 'artefact')
+            const data = await Resource_Finder_Nafig(inputs_storage, input, 'artefact', datenow, dateold, global_koef)
             if ( data.income < planet.artefact ) {
                 inputs_storage[data.counter].income += data.income
                 income_wil.artefact += data.income
@@ -121,5 +123,65 @@ async function Mine_Controller(context: Context, user: User, builder: Builder, i
                 console.error(`Ошибка: ${error.message}`);
             });
         }
+    }
+}
+
+async function Resource_Finder_Nafig_Outcome(input_storage: Input[], input_mine: Output, target: string, datenow: Date, dateold: Date, global_koef: number) {
+    const data = { income: 0, counter: 0 }
+    for (let i=0; i < input_storage.length; i++) {
+        const store = input_storage[i]
+        if (store.name == target) {
+            console.log(`${input_mine.name}: ${input_mine.outcome} * (${Number(datenow)} - ${Number(dateold)})/${input_mine.time}*${global_koef}=${input_mine.outcome * (Number(datenow)-Number(dateold))/input_mine.time*global_koef}`)
+            data.income = input_mine.outcome * (Number(datenow)-Number(dateold))/input_mine.time*global_koef
+            data.counter = i
+        }
+    }
+    return data
+}
+async function Powerstation_Controller(context: Context, user: User, builder: Builder, id_planet: number) {
+    const storage: Builder | null = await prisma.builder.findFirst({ where: { id_user: user.id, id_planet: id_planet, name: 'Склад' } })
+    if (!storage) { await Send_Message(context.peerId, 'Але, у вас склада нет на базе, вы дома?'); return }
+    const planet: Planet | null = await prisma.planet.findFirst({ where: { id: id_planet } })
+    if (!planet) { await Send_Message(context.peerId, 'Але, у вас планеты нет на базе, вы дома?'); return }
+    const datenow: Date = new Date()
+    const dateold: Date = new Date(builder.update)
+    const inputs_storage: Input[] = JSON.parse(storage.input)
+    const inputs_mine: Input[] = JSON.parse(builder.input)
+
+    let global_koef = 0
+    const requires: Require[] = JSON.parse(builder.require)
+    for (const require of requires) {
+        if (require.name == 'worker') {
+            const worker_check = await prisma.worker.count({ where: { id_builder: builder.id } })
+            global_koef = worker_check <= Math.floor(require.limit) ? worker_check/Math.floor(require.limit) : 1
+        }
+    }
+    const outputs: Output[] = JSON.parse(builder.output)
+    for (const output of outputs) {
+        if (output.name == 'coal') {
+            const data = await Resource_Finder_Nafig_Outcome(inputs_storage, output, 'coal', datenow, dateold, global_koef)
+            if ( data.income < inputs_storage[data.counter].income ) {
+                inputs_storage[data.counter].income -= data.income
+            }
+        }
+    }
+    for (const input of inputs_mine) {
+        if (input.name == 'energy') {
+            console.log(`${input.name}: ${input.income} * (${Number(datenow)} - ${Number(dateold)})/${input.time}*${global_koef}=${input.income * (Number(datenow)-Number(dateold))/input.time*global_koef}`)
+            const data = input.income * (Number(datenow)-Number(dateold))/input.time*global_koef
+            console.log(`Добавлено энергии: ${data}`)
+            await prisma.$transaction([
+                prisma.user.update({ where: { id: user.id }, data: { energy: { increment: data }, update: datenow } }),
+                prisma.builder.update({ where: { id: builder.id }, data: { update: datenow } }),
+                prisma.builder.update({ where: { id: storage.id }, data: { input: JSON.stringify(inputs_storage) } }),
+            ]).then(([]) => {
+                console.log('Успешная работа электростанции')
+            })
+            .catch((error) => {
+                //event_logger = `⌛ Произошла ошибка просчета добычи с шахты, попробуйте позже` 
+                console.error(`Ошибка: ${error.message}`);
+            });
+        }
+        
     }
 }
